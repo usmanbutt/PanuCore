cb40f5d24ca07f9dbd3a9dc6a6888c3ab6ec1cca
Pointerxx <marvin.wichmann@web.de>
2011-10-21 21:02:31 +0200

    CDB/SQL: Modifie Description of last Command
    Signed-off-by: Pointerxx <marvin.wichmann@web.de>

--- sql/ChaosEMU/world/2011.10.21.01_world_command.sql
+++ sql/ChaosEMU/world/2011.10.21.01_world_command.sql
@@ -1,2 +1,4 @@
 DELETE FROM command WHERE name = 'npc set emote';
-INSERT INTO command ('npc set emote', 3, 'Syntax: Target a NPC and Type .npc set emote  | Emote will be saved in Database');
+INSERT INTO command VALUES
+('npc set emote', 3, 'Syntax: .npc set emote $target \r\n\r\nSave the Emote in the Database and add the Creature this emote.');
+
