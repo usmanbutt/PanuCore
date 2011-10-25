bd40c85a4f73a1d718738aab9767fc5e1ba9b9e0
plangcoder <plangcoder@hotmail.com>
2011-10-25 19:49:15 +0330

    Fixed ice wall in Pit of Saron

--- src/server/scripts/Northrend/FrozenHalls/PitOfSaron/instance_pit_of_saron.cpp
+++ src/server/scripts/Northrend/FrozenHalls/PitOfSaron/instance_pit_of_saron.cpp
@@ -153,6 +153,18 @@ class instance_pit_of_saron : public InstanceMapScript
                         break;
                 }
             }
+            
+            void OnGameObjectCreate(GameObject* go)
+			{
+				switch (go->GetEntry())
+				{
+				case GO_ICE_WALL:
+					uiIceWall = go->GetGUID();
+					if(GetBossState(DATA_GARFROST) == DONE && GetBossState(DATA_ICK) == DONE)
+						HandleGameObject(NULL,true,go);
+					break;
+				}
+			}
 
             bool SetBossState(uint32 type, EncounterState state)
             {
@@ -161,6 +173,13 @@ class instance_pit_of_saron : public InstanceMapScript
 
                 switch (type)
                 {
+                    case DATA_ICK:
+                    	if (state == DONE)
+	                	{
+		                	if(GetBossState(DATA_GARFROST)==DONE)
+			                	HandleGameObject(uiIceWall,true,NULL);
+						}
+		                	break;
                     case DATA_GARFROST:
                         if (state == DONE)
                         {
@@ -171,6 +190,9 @@ class instance_pit_of_saron : public InstanceMapScript
                                 else
                                     summoner->SummonCreature(NPC_GORKUN_IRONSKULL_2, SlaveLeaderPos, TEMPSUMMON_MANUAL_DESPAWN);
                             }
+                            
+                            if(GetBossState(DATA_ICK)==DONE)
+                                     HandleGameObject(uiIceWall,true,NULL);
                         }
                         break;
                     case DATA_TYRANNUS:
@@ -286,6 +308,7 @@ class instance_pit_of_saron : public InstanceMapScript
             uint64 _tyrannusEventGUID;
             uint64 _jainaOrSylvanas1GUID;
             uint64 _jainaOrSylvanas2GUID;
+            uint64 uiIceWall;
 
             uint32 _teamInInstance;
         };
