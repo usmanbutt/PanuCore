5fb82b8004961372b392a748bbb9be6cbdbf0177
Locknes <locknes91@windowslive.com>
2011-10-30 20:33:47 +0100

    DB: fix Loot-Filled Pumpkin's loot

--- sql/ChaosEMU/world/2011.10.30_02_world_headlesshorseman.sql
+++ sql/ChaosEMU/world/2011.10.30_02_world_headlesshorseman.sql
@@ -10,8 +10,38 @@ INSERT INTO `script_texts` (`npc_entry`, `entry`, `content_default`, `content_lo
 
 -- Correct equip template
 REPLACE INTO `creature_equip_template` (`entry`, `itemEntry1`, `itemEntry2`, `itemEntry3`) VALUES (3001, 49128, 0, 0);
+UPDATE `creature_template` SET `equipment_id`=3001 WHERE `entry`=23682;
 
 -- Scriptnames
 UPDATE `creature_template` SET `ScriptName`='mob_pulsing_pumpkin' WHERE `entry` = 23694;
 UPDATE `creature_template` SET `ScriptName`='mob_wisp_invis', `faction_A`=35, `faction_H`=35, `unit_flags`='33554434' WHERE `entry`='23686';
 UPDATE `creature_template` SET `ScriptName`='mob_wisp_invis', `unit_flags`='33554434' WHERE `entry`='24034';
+
+-- Fix Loot-Filled Pumpkin's loot`creature_template_addon`
+DELETE FROM `item_loot_template` WHERE `entry` = 54516;
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 33292, 0, 1, 1, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 33154, 0, 1, 1, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 33176, 0, 1, 1, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 49128, 0, 1, 1, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 49126, 0, 1, 1, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 37012, 0, 1, 1, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 20569, 0, 1, 2, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 34003, 0, 1, 2, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 20572, 0, 1, 2, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 20567, 0, 1, 2, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 20391, 0, 1, 2, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 20570, 0, 1, 2, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 20573, 0, 1, 2, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 34000, 0, 1, 2, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 20562, 0, 1, 2, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 20392, 0, 1, 2, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 20565, 0, 1, 2, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 20563, 0, 1, 2, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 20571, 0, 1, 2, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 20574, 0, 1, 2, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 20561, 0, 1, 2, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 20564, 0, 1, 2, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 34001, 0, 1, 2, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 20568, 0, 1, 2, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 20566, 0, 1, 2, 1, 1);
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (54516, 34002, 0, 1, 2, 1, 1);
