38d82605d7012512f5598c39c01a918d3bbd16cd
Locknes <locknes91@windowslive.com>
2011-11-21 11:49:48 +0100

    DB: Add chest spawn for Hodir's rare cache

--- sql/ChaosEMU/world/2011.11.21_00_world_misc.sql
+++ sql/ChaosEMU/world/2011.11.21_00_world_misc.sql
@@ -1,4 +1,3 @@
--- INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES (2712204, 194200, 603, 1, 1, 2036.11, -201.289, 432.687, 3.17535, 0, 0, 0.999858, -0.0168795, -604800, 255, 1);
 UPDATE `creature_template` SET `ScriptName`='npc_spring_rabbit' WHERE `entry`=32791;
 UPDATE `achievement_criteria_data` SET `value1`=186 WHERE `criteria_id`=9199 AND `type`=6;
 -- Fix Quest 'The Tome of Divinity' part 7 (1768)
